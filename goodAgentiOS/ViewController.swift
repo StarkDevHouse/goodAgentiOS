f7cc27408b11032be1500974b5bd32030f56ba72
Junmo Kang <keaton@starkcorp.co.kr>
2017-04-21 09:28:16 +0900

    Initial Commit

---
+++ goodAgentiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goodAgentiOS
+//
+//  Created by Junmo Kang on 21/04/2017.
+//  Copyright © 2017 Kalabash Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
