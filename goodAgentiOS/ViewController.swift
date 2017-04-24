2bc9110370e901e435a574587e935848a9df7825
Junmo Kang <keaton@starkcorp.co.kr>
2017-04-24 16:07:22 +0900

    단일 뷰컨트롤러를 두개로 나누기. 두 화면이 같아보이게 하여 화면 전환 (Appering, Disappearing) 사이 간극이 없게 함

--- goodAgentiOS/ViewController.swift
+++ goodAgentiOS/ViewController.swift
@@ -12,60 +12,60 @@ class ViewController: UIViewController {
 
     //MARK: Properties
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var sideMenu: UIView!
-    @IBOutlet weak var darkenBox: UIImageView!
+    // @IBOutlet weak var sideMenu: UIView!
+    // @IBOutlet weak var darkenBox: UIView!
     @IBOutlet weak var agentWrapper: UIScrollView!
     
     
     
     
-    //MARK: Call SideBar
-    @IBAction func menuButton(_ sender: UIButton) {
-        if (sideMenu.frame.origin.x < 0) {
-            moveMenu(Bool: true)
-        } else {
-            moveMenu(Bool: false)
-        }
-    }
-    
-    @IBAction func hideMenuButton(_ sender: UIButton) {
-        moveMenu(Bool: false)
-    }
-    
-    func moveMenu(Bool: Bool) {
-        if (Bool == true) {
-            let Xpos = sideMenu.frame.origin.x + 300
-            let Ypos = sideMenu.frame.origin.y
-            
-            let height = sideMenu.frame.size.height
-            let width = sideMenu.frame.size.width
-            
-            UIView.animate(withDuration: 0.3) {
-                self.sideMenu.frame = CGRect(x: Xpos, y: Ypos, width: width, height: height)
-            }
-            UIView.animate(withDuration: 0.3) {
-                self.darkenBox.isHidden = false
-            }
-        } else {
-            let Xpos = sideMenu.frame.origin.x - 300
-            let Ypos = sideMenu.frame.origin.y
-            
-            let height = sideMenu.frame.size.height
-            let width = sideMenu.frame.size.width
-            
-            UIView.animate(withDuration: 0.3) {
-                self.sideMenu.frame = CGRect(x: Xpos, y: Ypos, width: width, height: height)
-            }
-            
-            UIView.animate(withDuration: 0.3) {
-                self.darkenBox.isHidden = true
-            }
-        }
-    }
+//    //MARK: Call SideBar
+//    @IBAction func menuButton(_ sender: UIButton) {
+//        if (sideMenu.frame.origin.x < 0) {
+//            moveMenu(Bool: true)
+//        } else {
+//            moveMenu(Bool: false)
+//        }
+//    }
+//    
+//    @IBAction func hideMenuButton(_ sender: UIButton) {
+//        moveMenu(Bool: false)
+//    }
+//    
+//    func moveMenu(Bool: Bool) {
+//        if (Bool == true) {
+//            let Xpos = sideMenu.frame.origin.x + 300
+//            let Ypos = sideMenu.frame.origin.y
+//            
+//            let height = sideMenu.frame.size.height
+//            let width = sideMenu.frame.size.width
+//            
+//            UIView.animate(withDuration: 0.3) {
+//                self.sideMenu.frame = CGRect(x: Xpos, y: Ypos, width: width, height: height)
+//            }
+//            UIView.animate(withDuration: 0.3) {
+//                self.darkenBox.isHidden = false
+//            }
+//        } else {
+//            let Xpos = sideMenu.frame.origin.x - 300
+//            let Ypos = sideMenu.frame.origin.y
+//            
+//            let height = sideMenu.frame.size.height
+//            let width = sideMenu.frame.size.width
+//            
+//            UIView.animate(withDuration: 0.3) {
+//                self.sideMenu.frame = CGRect(x: Xpos, y: Ypos, width: width, height: height)
+//            }
+//            
+//            UIView.animate(withDuration: 0.3) {
+//                self.darkenBox.isHidden = true
+//            }
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.darkenBox.isHidden = true
+        // self.darkenBox.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
