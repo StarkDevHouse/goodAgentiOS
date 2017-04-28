82f723f08e627b95766d50976a34fa0a262bc1c0
Junmo Kang <keaton@starkcorp.co.kr>
2017-04-28 19:43:51 +0900

    코드 재정비 중. 지도 보여주기는 아직 불안정 self.view.addSubView(mapView!) 에서 불안정

--- goodAgentiOS/initialVC.swift
+++ goodAgentiOS/initialVC.swift
@@ -9,21 +9,22 @@
 import UIKit
 import GoogleMaps
 
-class initialVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+class initialVC: UIViewController, UISearchBarDelegate {
 
-    //MARK: - Properties
+    //MARK: - Properties  , UITableViewDelegate, UITableViewDataSource,
     
-    @IBOutlet weak var tableView: UITableView!
-    
+
+    @IBOutlet weak var searchResultTable: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-//    @IBOutlet weak var map: UIView!
-
-    
+    var mapViewCtrl: GMSMapView?
     
     var searchController:UISearchController!
-    var searchActive : Bool = false
-    var data = ["San Francisco","New York","San Jose","Chicago","Los Angeles","Austin","Seattle"]
-    var filtered:[String] = []
+    var searching : Bool = false
+    var originalArray = ["San Francisco","New York","San Jose","Chicago","Los Angeles","Austin","Seattle"]
+    var searchingDataArray:[String] = []
+    var width = UIScreen.main.bounds.width
+    var height = UIScreen.main.bounds.height
+    
     let HQpos = [37.5466822, 127.0501807]
 
     //MARK: - Action: Call SideBar
@@ -31,59 +32,31 @@ class initialVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
     @IBAction func CallSideBar(_ sender: Any) {
 
         let uvc = self.storyboard!.instantiateViewController(withIdentifier: "SideBarViewController")
-        // uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
         self.present(uvc, animated: false)
 
     }
 
-    //MARK: - Load GoogleMap
-    
-    override func loadView() {
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureSearchController()
-//        self.searchBar = UISearchBar(frame: CGRect.init(x: 15, y: 20, width: 250, height: 44))
-//        self.searchBar.delegate = self
-//        
-        
-//        tableView.delegate = self
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        
-        
         let camera = GMSCameraPosition.camera(withLatitude: HQpos[0], longitude: HQpos[1], zoom: 15)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y:200, width: 414, height: 200), camera: camera)
         mapView.isMyLocationEnabled = true
         mapView.accessibilityElementsHidden = false
+        mapView.center = self.view.center
+
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: HQpos[0], longitude: HQpos[1])
+        marker.title = "6G HQ"
+        marker.snippet = "SeongDong"
+        marker.map = mapView
+
+        searchBar.delegate = self
         
-        // self.view = mapView
-        // self.map = mapView
-//
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: HQpos[0], longitude: HQpos[1])
-//        marker.title = "6G HQ"
-//        marker.snippet = "SeongDong"
-//        marker.map = mapView
-        
-        
-        // Setup delegates
-//        if (tableView != nil) {
-//            
-//        }
-        
-        
-        // self.view.addSubview(searchBar)
-        //self.searchBar.layer.cornerRadius = 0
-        
-//        if (searchBar != nil) {
-//            
-//
-//        } else {
-//            print("no search bar")
-//        }
+        mapViewCtrl?.center = self.view.center
+        self.view.addSubview(mapViewCtrl!)
         
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -94,74 +67,45 @@ class initialVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
         // Dispose of any resources that can be recreated.
     }
     
-    
-    // MARK: - search Ctrl.
-    func configureSearchController() {
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self as? UISearchResultsUpdating
-        searchController.dimsBackgroundDuringPresentation = true
-        searchController.searchBar.delegate = self
-    }
-    
-    
     // MARK: - Search Bar and Table View
 
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchActive = true
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        searchActive = false
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchActive = false
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchActive = false
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        filtered = data.filter({ (text) -> Bool in
-            let tmp: NSString = text as NSString
-            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
-            return range.location != NSNotFound
-        })
-        
-        if (filtered.count == 0) {
-            searchActive = false;
-        } else {
-            searchActive = true;
-        }
-        self.tableView.reloadData()
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        if(searchActive) {
-            cell.textLabel?.text = filtered[indexPath.row]
-        } else {
-            cell.textLabel?.text = data[indexPath.row]
-        }
-        
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(searchActive) {
-            return filtered.count
-        }
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
-    }
-    
-    
-
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // Meaning searching is on, then it's count searching array
+//        if searching == true {
+//            return searchingDataArray.count
+//        } else {
+//            return originalArray.count
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        
+//        if searching == true {
+//            cell.textLabel?.text = searchingDataArray[indexPath.row]
+//        } else {
+//            cell.textLabel?.text = originalArray[indexPath.row]
+//        }
+//        
+//        return cell
+//    }
+//
+//    func searchBar(_ seachBar: UISearchBar, textDidChange searchText: String) {
+//        searchingDataArray = originalArray.filter({ (text) -> Bool in
+//            let tmp: NSString = text as NSString
+//            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+//            return range.location != NSNotFound
+//        })
+//        
+//        if(searchingDataArray.count == 0) {
+//            searching = false
+//        } else {
+//            searching = true
+//        }
+//        self.searchResultTable.reloadData()
+//    }
 }
